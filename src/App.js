05e7b25128b53ba7bf90b27626e93c15fc0c6f5e
viblon <io.grigorev@physics.msu.ru>
2018-02-21 00:51:04 +0300

    first step

--- src/App.js
+++ src/App.js
@@ -1,87 +1,66 @@
+import 'antd/dist/antd.css'
 import React, { Component } from 'react';
-import './App.css';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-/*import Tabs, { TabPane } from 'rc-tabs';
-import TabContent from 'rc-tabs/lib/SwipeableTabContent';
-import ScrollableInkTabBar from 'rc-tabs/lib/ScrollableInkTabBar';*/
-import 'rc-tabs/assets/index.css';
-import 'react-tabs/style/react-tabs.css';
-/*const PanelContent = ({ id }) => (
-    <div>{[1, 2, 3, 4].map(item => <p key={item}>{id}</p>)}</div>
-);*/
+import {
+    Tabs,
+    Layout,
+    Input,
+    Icon,
+    Col,
+    Row,
+    Button,
+} from 'antd';
+const TabPane = Tabs.TabPane;
+const { Header, Footer, Content } = Layout;
 class App extends Component {
-/*    state = {
-        activeKey: '',
-        start: 0,
+    constructor(props) {
+        super(props);
+        this.state = {
+            userName: '',
+        };
+    }
+    emitEmpty = () => {
+        this.userNameInput.focus();
+        this.setState({ userName: '' });
     };
-
-    onChange = (activeKey) => {
-        console.log(`onChange ${activeKey}`);
-        this.setState({
-            activeKey,
-        });
+    onChangeUserName = (e) => {
+        this.setState({ userName: e.target.value });
     };
-
-    onTabClick = (key) => {
-        console.log(`onTabClick ${key}`);
-        if (key === this.state.activeKey) {
-            this.setState({
-                activeKey: '',
-            });
-        }
-    };
-
-    tick = () => {
-        this.setState({
-            start: this.state.start + 10,
-        });
-    };*/
 
     render() {
-        //const start = this.state.start;
-        return (
-          <div className="App">
-            <header className="App-header">
-              <h1 className="App-title">Welcome to React</h1>
-            </header>
-                <div className="Main">
-                    <p className="App-intro">
-                      To get started, edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    {/*<Tabs
-                        renderTabBar={() => <ScrollableInkTabBar onTabClick={this.onTabClick}/>}
-                        renderTabContent={() => <TabContent animatedWithMargin />}
-                        activeKey={this.state.activeKey}
-                        onChange={this.onChange}
-                    >
-                        <TabPane tab={`tab ${start}`} key="1">
-                            <PanelContent id={start} />
-                        </TabPane>
-                        <TabPane tab={`tab ${start + 1}`} key="2">
-                            <PanelContent id={start + 1} />
-                        </TabPane>
-                        <TabPane tab={`tab ${start + 2}`} key="3">
-                            <PanelContent id={start + 2} />
-                        </TabPane>
-                        <TabPane tab={`tab ${start + 3}`} key="4" disabled>
-                            <PanelContent id={start + 3} />
-                        </TabPane>
-                    </Tabs>*/}
-                    <Tabs>
-                        <TabList>
-                            <Tab>Title 1</Tab>
-                            <Tab>Title 2</Tab>
-                        </TabList>
+        const { userName } = this.state;
+        const suffix = userName ? <Icon type="close-circle" onClick={this.emitEmpty} /> : null;
+        const callback = (key) => {
+            console.log(key);
+        };
 
-                        <TabPanel>
-                            <h2>Any content 1</h2>
-                        </TabPanel>
-                        <TabPanel>
-                            <h2>Any content 2</h2>
-                        </TabPanel>
-                    </Tabs>
-                </div>
-          </div>
+        return (
+        <div>
+            <Layout>
+                <Header>Header</Header>
+                <Content>
+                    <Row>
+                        <Col span={12} offset={6}>
+                            <Tabs defaultActiveKey="1" onChange={callback}>
+                                <TabPane tab="Регистрация" key="1">
+                                    <Input
+                                        placeholder="Enter your username"
+                                        prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                        suffix={suffix}
+                                        value={userName}
+                                        onChange={this.onChangeUserName}
+                                        ref={node => this.userNameInput = node}
+                                    />
+                                    <Button type="primary">Submit</Button>
+                                </TabPane>
+                                <TabPane tab="Tab 2" key="2">Content of Tab Pane 2</TabPane>
+                                <TabPane tab="Tab 3" key="3">Content of Tab Pane 3</TabPane>
+                            </Tabs>
+                        </Col>
+                    </Row>
+                </Content>
+                <Footer>Footer</Footer>
+            </Layout>
+        </div>
         );
     }
 }
